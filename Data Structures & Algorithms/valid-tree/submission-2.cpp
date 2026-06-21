e64a6c9784f712bdf3da7105283f48891dcd1f11
Shreeram Prakash Hegde <161030477+Shreeramhegde01@users.noreply.github.com>
2026-06-21 21:45:39 +0530

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    bool dfs(vector<vector<int>>& adj, int i, int par,vector<bool>& vis){
+        if(vis[i]) return false;
+        vis[i]=true;
+        bool ok=true;
+        for(auto it:adj[i]){
+            if(it==par) continue;
+            ok=ok&&dfs(adj,it,i,vis);
+        }
+        return ok;
+    }
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+       vector<vector<int>> adj(n);
+       for(auto &it:edges){
+        adj[it[0]].push_back(it[1]);
+        adj[it[1]].push_back(it[0]);
+       }
+       vector<bool>vis(n,false);
+       bool ok=dfs(adj,0,-1,vis);
+       for(bool i:vis) ok=i&&ok;
+       return ok;
+    }
+};
