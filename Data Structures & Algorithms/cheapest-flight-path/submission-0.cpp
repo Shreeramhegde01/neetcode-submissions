892b41cb31f7acf46cee22006173de644e690b62
Shreeram Prakash Hegde <161030477+Shreeramhegde01@users.noreply.github.com>
2026-06-25 12:07:54 +0530

    Add: cheapest-flight-path - submission-0

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto &it:flights){
+            adj[it[0]].push_back({it[1],it[2]});
+        }
+        vector<int> dist(n,INT_MAX);
+        queue<tuple<int,int,int>> q;
+        q.push({0,src,0});
+        while(!q.empty()){
+            auto[stops,node,d]=q.front();
+            q.pop();
+            if(stops>k) continue;
+            for(auto &it:adj[node]){
+                int to=it.first;
+                int edjw=it.second;
+                if(d+edjw<dist[to]){
+                    dist[to]=d+edjw;
+                    q.push({stops+1,to,dist[to]});
+                }
+            }
+        }
+        return dist[dst]==INT_MAX?-1:dist[dst];
+    }
+};
