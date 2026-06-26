3ba23b174c25a887042c64d3d1711b157cb9e8bc
Shreeram Prakash Hegde <161030477+Shreeramhegde01@users.noreply.github.com>
2026-06-26 18:35:17 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    int f(vector<int>& nums,int i,vector<int>& dp){
+        if(i>=nums.size()) return 0;
+        if(dp[i]!=-1) return dp[i];
+        return dp[i]=max(f(nums,i+1,dp),nums[i]+f(nums,i+2,dp));
+    }
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(),-1);
+        return f(nums,0,dp);
+    }
+};
