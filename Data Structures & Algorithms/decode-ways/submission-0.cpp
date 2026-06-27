ef2e8a1c7d9ce9fe792b37c14db42fd1d3d8466e
Shreeram Prakash Hegde <161030477+Shreeramhegde01@users.noreply.github.com>
2026-06-27 13:52:13 +0530

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    int f(string &s, int i,vector<int>& dp){
+        if(i>=s.size()) return 1;
+        if(s[i]=='0') return 0;
+        if(dp[i]!=-1) return dp[i];
+        int ans=f(s,i+1,dp);
+        if(i<s.size()-1)
+            if(s[i]=='1' or (s[i]=='2' and s[i+1]<'7')) ans+=f(s,i+2,dp);
+        return dp[i]=ans;
+    }
+public:
+    int numDecodings(string s) {
+        vector<int> dp(s.size(),-1);
+        return f(s,0,dp);
+    }
+};
