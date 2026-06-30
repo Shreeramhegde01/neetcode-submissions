9936df3fc414a6611030777af3b926cc78441a73
Shreeram Prakash Hegde <161030477+Shreeramhegde01@users.noreply.github.com>
2026-06-30 11:41:39 +0530

    Add: longest-increasing-subsequence - submission-4

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+        for(int i=n-1;i>=0;i--){
+            for(int prev=i-1;prev>=-1;prev--){
+                int l=dp[i+1][prev+1];
+                if(prev==-1 or nums[i]>nums[prev]){
+                    l=max(l,1+dp[i+1][i+1]);
+                }
+                dp[i][prev+1]=l;
+            }
+        }
+        return dp[0][0];
+    }
+};
