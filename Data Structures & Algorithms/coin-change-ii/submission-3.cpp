dd53b7b12fb057151f164797c781dce6e5f23fee
Shreeram Prakash Hegde <161030477+Shreeramhegde01@users.noreply.github.com>
2026-07-01 11:28:05 +0530

    Add: coin-change-ii - submission-3

---
+++ Data Structures & Algorithms/coin-change-ii/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int f(int amount,vector<int>& coins, int i,vector<vector<int>> &dp){
+        if(amount==0) return 1;
+        if(i>=coins.size() or amount<0) return 0;
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int nottake=f(amount,coins,i+1,dp);
+        int take=0;
+        if(amount-coins[i]>=0)
+            take=f(amount-coins[i],coins,i,dp);
+        return dp[i][amount]=take+nottake;
+    }
+public:
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<int> cur(amount+1,0),next(amount+1,0);
+        cur[0]=next[0]=1;
+        for(int i=n-1;i>=0;i--){
+            for(int j=1;j<=amount;j++){
+                cur[j]=next[j];
+                if(j-coins[i]>=0){
+                    cur[j]+=cur[j-coins[i]];
+                }
+            }
+            next=cur;
+        }
+        return cur[amount];
+    }
+};
