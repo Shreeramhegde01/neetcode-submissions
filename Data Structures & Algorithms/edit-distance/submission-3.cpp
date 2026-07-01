a841386125df3ce950ff4bd9d50ff43880811e7a
Shreeram Prakash Hegde <161030477+Shreeramhegde01@users.noreply.github.com>
2026-07-01 22:44:22 +0530

    Add: edit-distance - submission-3

---
+++ Data Structures & Algorithms/edit-distance/submission-3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    int f(string &s1, string &s2, int i, int j, vector<vector<int>>& dp){
+        if(j<0) return i+1;
+        if(i<0) return j+1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i]==s2[j]) return dp[i][j]=f(s1,s2,i-1,j-1,dp);
+        int l=f(s1,s2,i-1,j,dp);
+        int m=f(s1,s2,i-1,j-1,dp);
+        int n=f(s1,s2,i,j-1,dp);
+        return dp[i][j]=1+min({l,m,n});
+    }
+public:
+    int minDistance(string s1, string s2) {
+        int n=s1.size(),m=s2.size();
+        vector<int> cur(m+1,0),prev(m+1,0);
+        for(int j=0;j<=m;j++) prev[j]=j;
+        for(int i=1;i<=n;i++){
+            cur[0]=i;
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]) cur[j]=prev[j-1];
+                else {
+                    cur[j]=1+min({prev[j-1],prev[j],cur[j-1]});
+                }
+            }
+            prev=cur;
+        }
+        return  prev[m];
+    }
+};
